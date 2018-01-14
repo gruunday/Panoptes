109f9e5efc4992a8d8cc3e40f6199d4c5b59f893
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2018-01-14 10:42:31 +0000

    tidyed up comments and  prints

--- code/panoptes.py
+++ code/panoptes.py
@@ -3,14 +3,6 @@
 import os
 import imp
 
-#def load_plugins(self):
-#    self.plugins = []
-#    for plug in self.list_plugins():
-#        self.plugins.append(plug)
-#        f, filename, description = imp.find_module(plug, ['plugins'])
-#        imp.find_module('ssid_detection', ['plugins'])
-#        imp.load_module(plug, f, filename, description)
-
 def lst_plugins(directory='plugins'):
     for fi in os.listdir(directory):
         if not fi.endswith(".py"):
@@ -20,7 +12,6 @@ def lst_plugins(directory='plugins'):
 
 def run():
     for plugin in lst_plugins():
-        print(plugin)
         plugin.command('start')
 
 if __name__ == '__main__':
