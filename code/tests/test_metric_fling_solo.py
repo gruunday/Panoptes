06a8674d170c0511ebeb708d752c23f2c900d553
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2018-01-27 17:59:00 +0000

    debugging

--- code/tests/test_metric_fling_solo.py
+++ code/tests/test_metric_fling_solo.py
@@ -2,15 +2,21 @@
 
 import os
 import subprocess as sp
+import random 
+from time import strftime
 from metric_fling import Metric_Fling
 import unittest
 
 class Metric_Fling_TestCase(unittest.TestCase):
     """Test Case class for metric_fling daemon"""
+    def setUp(self):
+        f = open('/tmp/metrix.log', 'a+')
+        for _ in range(20):
+            f.write(f'fake.metrics.local {random.randint(0,100)} {strftime("%H:%M:%S")}')
+        f.close()
 
     def test_solo_start(self):
         """Test Case D Solo metric fling startup"""
-
         # Start system 
         os.system('python3.6 metric_fling.py start')
         os.system('echo \"\n\" | ps aux | grep metric')
