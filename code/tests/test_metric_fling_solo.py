6413c2146b1253d40543ec373ad02eab82a3859b
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2018-01-27 18:37:40 +0000

    tidy up debuging

--- code/tests/test_metric_fling_solo.py
+++ code/tests/test_metric_fling_solo.py
@@ -19,24 +19,18 @@ class Metric_Fling_TestCase(unittest.TestCase):
         """Test Case D Solo metric fling startup"""
         # Start system 
         os.system('python3.6 metric_fling.py start')
-        os.system('echo \"\n\" | ps aux | grep metric')
-        os.system('cat /var/log/panoptes/system.log')
         aux_out = str(sp.check_output(['ps','aux'])).count('python3.6 metric_fling.py')
         self.assertGreaterEqual(aux_out, 1, 'Start test failed')
-#        print(str(sp.check_output(['ps','aux'])))
 
         # Restart system
         os.system('python3.6 metric_fling.py restart')
         aux_out = str(sp.check_output(['ps','aux'])).count('python3.6 metric_fling.py')
         self.assertGreaterEqual(aux_out, 1, 'Restart test failed')
 
-        print(str(sp.check_output(['ps','aux'])).count('metric'))
         # Stop system
         os.system('python3.6 metric_fling.py stop')
         aux_out = str(sp.check_output(['ps','aux'])).count('python3.6 metric_fling.py')
         self.assertEqual(aux_out, 0, 'Stop test failed')
-
-        print(str(sp.check_output(['ps','aux'])).count('metric'))
 
 if __name__ == '__main__':
     test_solo_start()
