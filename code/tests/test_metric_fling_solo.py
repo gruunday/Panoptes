19268e010e1f7e5c4663ae4be0ee59d8435a933e
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2018-01-27 17:23:22 +0000

    Added debuging print

--- code/tests/test_metric_fling_solo.py
+++ code/tests/test_metric_fling_solo.py
@@ -15,17 +15,20 @@ class Metric_Fling_TestCase(unittest.TestCase):
         os.system('python3.6 metric_fling.py start')
         aux_out = str(sp.check_output(['ps','aux'])).count('python3.6 metric_fling.py')
         self.assertGreaterEqual(aux_out, 1, 'Start test failed')
+        print(str(sp.check_output(['ps','aux'])).count('metric'))
 
         # Restart system
         os.system('python3.6 metric_fling.py restart')
         aux_out = str(sp.check_output(['ps','aux'])).count('python3.6 metric_fling.py')
         self.assertGreaterEqual(aux_out, 1, 'Restart test failed')
 
+        print(str(sp.check_output(['ps','aux'])).count('metric'))
         # Stop system
         os.system('python3.6 metric_fling.py stop')
         aux_out = str(sp.check_output(['ps','aux'])).count('python3.6 metric_fling.py')
         self.assertEqual(aux_out, 0, 'Stop test failed')
 
+        print(str(sp.check_output(['ps','aux'])).count('metric'))
 
 if __name__ == '__main__':
     test_solo_start()
