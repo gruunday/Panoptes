f83d6325d70cbd88c1ea8ce34e34d77b5d7b8b42
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2018-02-10 18:38:35 +0000

    changed name to influence order

--- code/tests/test_node_up.py
+++ code/tests/test_node_up.py
@@ -8,24 +8,27 @@ import unittest
 
 class Nodes_Up_TestCase(unittest.TestCase):
     """Test Nodes Up"""
-    def test_nodes_up_start(self):
+    
+    def test01_nodes_up_start(self):
         """Test Nodes up start"""
         ans = command('start')
         self.assertEqual('Starting', ans)
  
-    def test_nodes_up_start(self):
+    def test02_nodes_up_start(self):
         """Test Nodes up restart"""
         with self.assertRaises(SystemExit) as cm:
             command('restart')
         self.assertEqual(cm.exception.code, 0)
    
-    def test_nodes_up_stop(self):
+    def test03_nodes_up_stop(self):
         """Test Nodes stop"""
         ans = command('stop')
         self.assertEqual('Stopped', ans)
 
-    def test_unknown_command(self):
+    def test04_unknown_command(self):
         """Test Nodes up bad command"""
         with self.assertRaises(SystemExit) as cm:
             command('hoogle boodle')
         self.assertEqual(cm.exception.code, 2)
+
+
