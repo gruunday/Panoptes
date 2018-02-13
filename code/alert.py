ac602c360582ef9792c601adbc64e17ca7bda821
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2018-02-13 22:58:02 +0000

    Use json config

--- code/alert.py
+++ code/alert.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3.6
 
 from slackclient import SlackClient
-from config import slack_token, slack_channel
+import json
 
-def slack_alert(message, channel=slack_channel):
-    sc = SlackClient(slack_token)
-    sc.api_call('chat.postMessage', channel=channel, 
-                text=message, username='Panoptes Alerts',
-                icon_emoji=':robot_face:')
+class Alert():
+
+    def __init__(self):
+        config = self.read_config()
+        self.slack_token = config["slack"]["slack_token"] 
+        self.slack_channel = config["slack"]["slack_channel"] 
+        self.slack_username = config["slack"]["slack_username"] 
+        self.slack_emoji = config["slack"]["slack_emoji"] 
+
+    def read_config(self):
+        with open('config.json') as json_config:
+            return json.load(json_config)
+
+    def _slack_alert(self, message):
+        sc = SlackClient(self.slack_token)
+        sc.api_call('chat.postMessage', channel=self.slack_channel, 
+                    text=message, username=self.slack_username,
+                    icon_emoji=self.slack_emoji)
+
+def slack_alert(message):
+    alert = Alert()
+    alert._slack_alert(message)
