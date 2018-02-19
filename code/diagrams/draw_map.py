99113dea025917d81e4c887827372ad027995711
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2018-02-19 12:38:10 +0000

    Updated simulated annealing code to generate diagrams

--- code/diagrams/draw_map.py
+++ code/diagrams/draw_map.py
@@ -11,6 +11,10 @@ width = 500
 seed(randint(1,1000))
 
 
+#
+# Start cost functions
+#
+
 def point_distance(point, other):
     #print(point, other)
     return math.sqrt((point[0] - other[0])**2 + (point[1] - other[1])**2)
@@ -42,10 +46,24 @@ def get_angles(node_loc):
             if node == other:
                 continue
             angle = nice_angle(node_loc[node][0], node_loc[other][0])
-            cost += abs(ideal_angle - angle)
-    return cost**2
+            #cost += abs(abs(ideal_angle) - abs(angle))
+            if abs(abs(angle) - ideal_angle) < .2:
+                cost += 600
+    return cost * 2
 
-def too_close(node_loc):
+def same_line(node_loc):
+    cost = 0
+    for node in node_loc:
+        for other in node_loc:
+            if node != other:
+                if abs(node_loc[node][0][0] - node_loc[other][0][0]) < 10:
+                    cost += 1000
+                if abs(node_loc[node][0][1] - node_loc[other][0][1]) < 10:
+                    cost += 9000
+    return cost * 2
+                
+
+def too_close(node_loc, server_loc=(250,250)):
     cost = 0
     # Distance that will be too close
     min_dist = 100
@@ -53,19 +71,24 @@ def too_close(node_loc):
         for other in node_loc:
             if node == other:
                 continue
+            # Too close to another
             dist = point_distance(node_loc[node][0], node_loc[other][0])
             if dist < min_dist:
-                cost += 200 * (100-dist)
+                cost += 100 * (100-dist)
             # too close to edge
             if (node_loc[node][0][0] - 500) < min_dist:
-                cost += abs(200 * (0.5 * (node_loc[node][0][0] - 500)))
+                cost += abs(50 * (0.5 * (node_loc[node][0][0] - 500)))
             if (node_loc[node][0][1] - 500) < min_dist:
-                cost += abs(200 * (0.5 * (node_loc[node][0][1] - 500)))
+                cost += abs(50 * (0.5 * (node_loc[node][0][1] - 500)))
             if (node_loc[node][0][0]) < min_dist:
-                cost += abs(200 * (0.5 * (node_loc[node][0][0])))
+                cost += abs(50 * (0.5 * (node_loc[node][0][0])))
             if (node_loc[node][0][1]) < min_dist:
-                cost += abs(200 * (0.5 * (node_loc[node][0][1])))
-    return cost
+                cost += abs(50 * (0.5 * (node_loc[node][0][1])))
+    # too close to server
+    if point_distance(server_loc, node_loc[node][0]) < 100:
+        cost += 100
+
+    return int(cost / 2)
 
 def off_map(node_lst, h=500, w=500):
     cost = 0
@@ -75,6 +98,26 @@ def off_map(node_lst, h=500, w=500):
             cost += 500
         if point[1] > h or point[1] < 0:
             cost += 500
+    return int(cost /2) 
+
+def same_slope(node_lst, centre_x=250, centre_y=250):
+    cost = 0
+    for node in node_lst:
+        for other in node_lst:
+            if other != node:
+                x = node_lst[node][0][0]
+                y = node_lst[node][0][1]
+                x1 = node_lst[other][0][0]
+                y1 = node_lst[other][0][1]
+                # Ensure no div 0
+                if (centre_x - x): slope1 = (centre_y - y) / (centre_x - x)
+                else: cost += 2000; continue
+                # Ensure no div 0
+                if (centre_x - x1): slope2 = (centre_y - y1) / (centre_x - x1)
+                else: cost += 2000; continue
+                # Check same slope
+                if slope1 == slope2: cost += 3000
+                elif abs(slope1 - slope2) < .5: cost += 2000
     return cost
 
 def cost(node_lst):
@@ -82,7 +125,14 @@ def cost(node_lst):
     cost += too_close(node_lst)
     cost += off_map(node_lst)
     cost += get_angles(node_lst)
+    #cost += same_line(node_lst)
+    cost += same_slope(node_lst)
     return cost
+
+#
+# End cost functions
+#
+
 
 def get_data():
     r = requests.get('http://panoptes.xyz:8080/render?target=*.ping.avg&format=json&from=-1min')
@@ -102,9 +152,16 @@ def give_points(node_lst):
     points = {}
     for node in node_lst:
         directions = [(1,1),(-1,1),(1,-1),(-1,-1)]
+        # Choose a direction
         direction = directions[randint(0,3)]
-        step_x = (direction[0] * node[1]) * (randint(0,3) + 3)
-        step_y = (direction[1] * node[1]) * (randint(0,3) + 3)
+        # Random X coordinate
+        step_x = (direction[0] * node[1]) 
+        if step_x > 0: step_x +=  (randint(0,150))
+        else: step_x -= (randint(0,150))
+        # Random Y coordinate
+        step_y = (direction[1] * node[1])
+        if step_y > 0: step_y += (randint(0,150))
+        else: step_y -= (randint(0,150))
         points[node[0]] = [(250 + step_x,\
                             250 + step_y), (250, 250)]
     return points
@@ -117,7 +174,8 @@ def draw_map(point, cost, width=500, height=500):
         draw.line((point[node][0],point[node][1]),fill=(0,255,0),width=3)
 
     for node in point:
-        draw.text(point[node][0],node,(0,0,0))
+        msg = node #+ str(point[node][0])
+        draw.text(point[node][0],msg,(0,0,0))
    
     # Draw server last to be on top
     server_pos = ((height/2)+3, (width/2)+3)
@@ -132,16 +190,35 @@ def main():
     #data = get_data()
     population = []
     data = ''
+    best_score = 1000000000000
     node_lst = get_nodes(data)
+    # make first random guess
     best_guess = give_points(node_lst)
-    #print(node_loc)
+    # get the cost of that guess
     score = cost(best_guess)
-    draw_map(best_guess, score)
+    #draw_map(best_guess, score)
+    # Run simulated_annealing on it to get it better
     ans = simulated_annealing(best_guess, cost)
-    #for graph in population:
-     #   print(graph, cost(graph))
-    score = cost(ans)
-    draw_map(ans, score)
+    # get score of new diagram
+    sim_score = cost(ans)
+    drawn = False
+    for _ in range(0,1):
+        if sim_score < 160000:
+            draw_map(ans, sim_score)
+            drawn = True
+            break
+        else:
+            if sim_score < best_score:
+                best_score = sim_score
+                best_guess = ans
+                print(f'using graph with score {best_score}')
+                simulated_annealing(ans, cost)
+            else:
+                print(f'Reusing graph score {best_score}')
+                simulated_annealing(best_guess, cost)
+    if not drawn:
+        draw_map(ans, best_score)
+    
 
 if __name__ == '__main__':
     main()
--- code/diagrams/simulated_aneal.py
+++ code/diagrams/simulated_aneal.py
@@ -5,25 +5,33 @@ def small_change(best_guess, space=500, max_step=50):
     #take into account dict
     #print(best_guess['anam'])
     for node in best_guess:
-        if rand.randint(1,100) < 60:
+        # only choose a couple to change
+        if rand.randint(1,100) > 80:
             continue
         x = best_guess[node][0][0]
         y = best_guess[node][1][1]
-        rand_num = rand.randint(1,150)
-        if rand.randint(1,50) < 25: rand_num = -rand_num
-        if rand.randint(1,50) < 25: x += rand_num
-        rand_num = rand.randint(1,150)
-        if rand.randint(1,50) < 25: rand_num = -rand_num
-        if rand.randint(1,50) < 25: y += rand_num
+        # Choose random number
+        rand_num = rand.randint(1,20)
+        # maybe negate random number
+        if rand.randint(0,100) < 50:
+            rand_num *= -1
+            # add or subtract random number 
+        x += rand_num
+        # Steps repeat for y value
+        rand_num = rand.randint(1,100)
+        if rand.randint(0,100) < 50:
+            rand_num *= -1
+        y += rand_num
+        # check if we go out of bands
         if x > space: x = space - 100 
         if x < 0: x = 0 
         if y > space: y = space - 100
         if y < 0: y = 0 
+        # replace value
         best_guess[node][0] = (x, y)
-    #print(best_guess)
     return best_guess
 
-def simulated_annealing(best_guess, cost, space=500, initial_T=100.0, final_T=0.1, cooling_rate=0.9999, max_step=1):
+def simulated_annealing(best_guess, cost, space=500, initial_T=100.0, final_T=0.1, cooling_rate=0.99, max_step=50):
 
     for step in range(max_step, 0, -1):
         T = initial_T
@@ -33,13 +41,12 @@ def simulated_annealing(best_guess, cost, space=500, initial_T=100.0, final_T=0.
             new = small_change(best_guess, space, step)
 
             # Check if it is better
-            score = cost(new) - cost(best_guess)
-            #print(new_cost)
-            if score < 0:
+            score = cost(best_guess) - cost(new)
+            if score >= 0:
                 best_guess = new
             else:
                 prob = math.e ** (-score / T) # Boltzmann Function
-                if rand.random() < prob:
+                if rand.random() < (prob):
                     best_guess = new # Even if it is worse might take it
 
             # Decrease Temp
