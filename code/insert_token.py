acc8310d5b6c58264697685aa6ce63c4a0919254
Tom Doyle <thomas.doyle9@mail.dcu.ie>
2018-03-07 14:34:42 +0000

    Update api key

--- code/insert_token.py
+++ code/insert_token.py
@@ -1,11 +1,15 @@
 import sys
 import json
 
-token = sys.argv[1]
+slack_token = sys.argv[1]
+pushbullet_token = sys.argv[2]
+pushbullet2_token = sys.argv[3]
+
 with open('config.json', 'r') as json_config:
     config = json.load(json_config)
 
-config["slack"]["slack_token"] = token
+config["slack"]["slack_token"] = slack_token
+config["pushbullet"]["push_device_tokens"] = [pushbullet_token, pushbullet2_token]
 
 with open('config.json', 'w') as json_config:
     json.dump(config, json_config)
